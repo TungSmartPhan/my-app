e96cb8093985ef21e32a78bfd380b2bb16f308b2
Tùng <tungptgcd18390@fpt.edu.vn>
2024-08-21 11:36:03 +0700

    new change 2024 August 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+// import "./styles.css";
+// import Sidebar from "./components/Sidebar";
+// import Dashboard from "./components/Dashboard";
+// import Orders from "./components/Orders";
+// import Products from "./components/Products";
+import Customers from "./components/Customers";
+import Header from "./headerComponent/Header"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-2 bg-dark min-height-100vh">
+          {/* <Sidebar /> */}
+        </div>
+        <main className="col-10">
+   <Header/>
+          <Routes>
+            {/* <Route path="/" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/products" element={<Products />} /> */}
+            <Route path="/customers" element={<Customers />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 }
-
-export default App;
